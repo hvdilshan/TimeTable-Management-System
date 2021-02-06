86aa0c3f621cae57a0707ec004e3c248235ddfbd
Viranga Dilshan <61415991+hvdilshan@users.noreply.github.com>
2021-02-06 21:36:00 +0530

    project

---
+++ src/com/spm/timetablemanagement/location/building.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.spm.timetablemanagement.location;
+
+import com.spm.timetablemanagement.form.room.*;
+import com.spm.timetablemanagement.util.DBconnection;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ysraf
+ */
+public class building {
+    
+
+    Connection c;
+    ResultSet rs;
+    PreparedStatement statement;
+    Statement st;
+    
+
+    public void building() {
+        try {
+            
+            c = DBconnection.getConnection();
+            st = c.createStatement();
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(InsertBuilding.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null , "Cannot configure database"+ex.getMessage(), "Database Error", JOptionPane.ERROR_MESSAGE);
+        } catch (IOException ex) {
+            Logger.getLogger(building.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null , "Cannot configure database"+ex.getMessage(), "Database Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+}
